c04ba722a1a979ad7ea756065d544fc344b0c3ad
Ross Brown <ross@cremalab.com>
2016-09-06 15:31:22 -0500

    replacing lory with ideal-image-slider

--- public/js/images.js
+++ public/js/images.js
@@ -1,36 +1,35 @@
-const lory = require('lory.js').lory
 require('image-zoom')
-
-
-document.addEventListener('DOMContentLoaded', () => {
-  const slider = document.querySelector('.project-gallery')
-  if (!slider) { return }
-  const navItems = document.querySelectorAll('.project-gallery-nav-item')
-  const setSlideClasses = (currentSlide) => {
-    Array.prototype.slice.call(navItems).forEach((el) => {
-      if (el.getAttribute('data-loop-index') == currentSlide) {
-        el.classList.add('active')
-      } else {
-        el.classList.remove('active')
-      }
-    })
+const $nav = document.querySelector('.project-gallery-nav-meta')
+const slider = new IdealImageSlider.Slider({
+  selector: '.project-gallery-frame',
+  height: 400,
+  width: 400,
+  effect: 'fade',
+  transitionDuration: 400,
+  interval: 4000,
+  afterChange: () => {
+    setSlideClasses(parseInt(slider.get('currentSlide').getAttribute('data-index')))
   }
+})
 
-  const handleSlideChange = (e) => {
-    const currentSlide = e.detail.currentSlide
-    setSlideClasses(currentSlide)
-  }
+const navItems = document.querySelectorAll('.project-gallery-nav-item')
+const setSlideClasses = (currentSlide) => {
+  Array.prototype.slice.call(navItems).forEach((el) => {
 
-  slider.addEventListener('after.lory.slide', handleSlideChange)
-  const lorySlider = lory(slider)
-  window.sliderr = lorySlider
-  slider.addEventListener('click', (e) => {
-    if ( e.srcElement.classList.contains('project-gallery-nav-item') ) {
-      e.preventDefault()
-      lorySlider.slideTo(parseInt(e.srcElement.getAttribute('data-loop-index')))
-      e.stopPropagation()
+    if (el.getAttribute('data-loop-index') == currentSlide) {
+      el.classList.add('active')
+    } else {
+      el.classList.remove('active')
     }
   })
+}
 
-  setSlideClasses(0)
+$nav.addEventListener('click', (e) => {
+  if ( e.srcElement.classList.contains('project-gallery-nav-item') ) {
+    e.preventDefault()
+    slider.gotoSlide(parseInt(e.srcElement.getAttribute('data-loop-index')))
+    e.stopPropagation()
+  }
 })
+setSlideClasses(1)
+slider.start()
